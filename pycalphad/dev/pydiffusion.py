b7ade727eba8dd5eab770eb274994e48be46b1f9
zhongjingjogy <zhongjingjogy@126.com>
2015-11-01 09:47:47 +0800

    to do list for a dictra-like module

---
+++ pycalphad/dev/pydiffusion.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+"""
+TODO:
+Here comes the todo list for developing dictra-like module to deal with
+substitutional phase (only one valid sublattice).
+1. implement the module to calculate interdiffusivities from the atomic mobility.
+2. a simulation module to simulate the diffusion couple with the thermodynamic
+database and kinetic database.
+3. new module for optimizing the interaction parameter in atomic mobility 
+according to the interdiffusivities obtained  from others.
+
+"""
